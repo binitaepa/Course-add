985d9ad2f72b8d0ce2a4bede1c93526cfae8a9fa
binitaepa <binitaepa@gmail.com>
2023-09-15 08:09:01 +0600

    Loaded all the course card also design is completed

--- src/components/Carts/Carts.jsx
+++ src/components/Carts/Carts.jsx
@@ -1,9 +1,20 @@
 import PropTypes from 'prop-types';
+import './Carts.css'
 
 const Carts = props => {
     return (
         <div>
-            <h1>this is cart section</h1>
+            <h1 className='credit'>Credit hour Remaining</h1>
+            <hr />
+            <div>
+                <p>Course Name</p>
+                <hr />
+                <div><ul>
+                    </ul></div>
+                    <hr />
+                    <p>Total credit hour: </p>
+            </div>
+            
         </div>
     );
 };
--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -3,36 +3,44 @@
 import './Courses.css'
 import Carts from '../Carts/Carts';
 import { useEffect, useState } from 'react';
-
+import { FaDollarSign } from "react-icons/fa";
+import { FaBookOpen } from "react-icons/fa";
 
 
 const Courses = () => {
     const [allCourses, setAllCourses] = useState([]);
+    const [selectedCourse, setSelectedCourse] = useState([]);
     useEffect(() => {
         fetch("./course-data.json")
           .then((res) => res.json())
           .then((data) => setAllCourses(data));
       }, []);
+     
+      const handleSelectedCourse=(courses)=>{
+        setSelectedCourse([...selectedCourse,courses]);
+
+      }
     
     return (
         <div className='main-container'>
         <div className='course-container'>
             {
-                allCourses.map((course)=>(<div key={course.id} >
-                <div className="card-img">
+                allCourses.map((course)=>(<div key={course.id} className='course-card' >
+                <div className="image">
                   <img  src={course.img} alt="" />
                 </div>
-                <h2>{course.course_name}</h2>
-                <p>
+                <h2 className='title'>{course.course_name}</h2>
+                <p className='description'>
                   <small>
                     {course.description}
                   </small>
                 </p>
                 <div className="info">
-                  <p>salary:{course.price} $</p>
-                  <p>{course.credit}</p>
+                  <p><FaDollarSign></FaDollarSign>Price:{course.price}$ </p>
+                  <p> Credit:{course.credit}hr <FaBookOpen></FaBookOpen></p>
                 </div>
                 <button
+                onClick={()=>handleSelectedCourse(course)}
                   
                   className="card-btn"
                 >
@@ -43,7 +51,7 @@ const Courses = () => {
                     ))
             }
         </div>
-        <div className='course-details-container'><Carts></Carts></div>
+        <div className='course-details-container'><Carts selectedCourse={selectedCourse}></Carts></div>
                 </div>
     );
 };
